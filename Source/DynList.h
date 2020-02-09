9b6a2d744b9bbfa9fae1f6dc4c6ae039da4b8957
William Delo <wdelo32@gmail.com>
2020-02-08 22:46:40 -0500

    Added copyArray function header, added title/description header comment.

--- Source/DynList.h
+++ Source/DynList.h
@@ -1,27 +1,37 @@
 #pragma once
 
+/** 
+	This is a dynamic list template class with the ability to 
+	push and pop elements both from the back and the front of
+	the list. Users can add and remove elements at specified
+	indices. 
+
+	@author William Delo
+	@version 8.2.2020
+*/
+
+
 template <class T>
 class DynList {
 public:
 	DynList();
 	~DynList();
-	int length() const;
-	void pushBack(const T& _T);
-	void pushFront(const T& _T);
+	int length() const;									
+	void pushBack(const T& _T);							
+	void pushFront(const T& _T);						
 	void popFront();
 	void popBack();
 	void add(const T& _T, const unsigned int index);
 	void remove(const unsigned int index);
 	void clear();
 	void reserve(const unsigned int reservation);
-	T& operator[](unsigned int index) const;
+	T& operator[](unsigned int index);
 
 private:
 	T* m_pArray;
 	unsigned int m_iLength;
 	unsigned int m_iReservedLength;
 	unsigned int m_iStartIndex;
+
+	void copyArray(T*& pCopyArray, unsigned int iStartIndex);
 };
-
-
-
